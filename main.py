f5c87f19e1b32f9a1b1500224da9ee599f8a4cf6
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2023-02-18 15:30:20 -0600

    wip lol

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math import cos, pi
+
 from PIL import Image
 from PIL import ImageDraw
 
@@ -6,7 +8,9 @@ INPUT_FILE_PATTERN = 'originals/{}.png'
 OUTPUT_FILE_PATTERN = 'combined/{}_{}.png'
 
 DIM = 76
-
+R = DIM / 2
+UPPER_PATH = [(0, 0), (R*cos(5 * pi/6)+R, R), (R, R), (R*cos(pi/6)+R, R), (2*R, 0), (0, 0)]
+LOWER_PATH = [(DIM, DIM), (DIM, 0), (0, DIM), (DIM, DIM)]
 
 def make_polygon(path):
     """
@@ -23,20 +27,18 @@ def run():
     for elem in ELEMENTS:
         upper_image = Image.open(INPUT_FILE_PATTERN.format(elem)).resize((DIM, DIM))
 
-        upper_path = [(0, 0), (0, DIM), (DIM, 0), (0, 0)]
-        upper_triangle = make_polygon(upper_path)
+        upper_triangle = make_polygon(UPPER_PATH)
 
         for elem2 in ELEMENTS:
             if elem2 == elem:
                 continue
             lower_image = Image.open(INPUT_FILE_PATTERN.format(elem2)).resize((DIM, DIM))
 
-            lower_path = [(DIM, DIM), (DIM, 0), (0, DIM), (DIM, DIM)]
-            lower_triangle = make_polygon(lower_path)
+            lower_triangle = make_polygon(LOWER_PATH)
 
             new = Image.new('RGBA', (DIM, DIM))
             new.paste(upper_image, (0, 0), mask=upper_triangle)
-            new.paste(lower_image, (0, 0), mask=lower_triangle)
+            #     new.paste(lower_image, (0, 0), mask=lower_triangle)
             new.save(OUTPUT_FILE_PATTERN.format(elem, elem2))
 
 
